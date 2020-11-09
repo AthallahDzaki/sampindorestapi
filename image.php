147e0e303352c315935c332092e14137538e8a3e
AthallahDzaki <discordgg5@gmail.com>
2020-11-09 13:27:14 +0700

    Edit image.php

--- image.php
+++ image.php
@@ -1,1 +1,9 @@
-
+<?php
+if(empty($_GET['text'])) echo 'Error Text Not Found', die;
+$image = imagecreate(200,80);
+$bg = imagecolorallocatealpha($image,255,255,255,0);
+$black = imagecolorallocate($image,255,255,255);
+imagettftext($image,11, 0, x, y, $black, "fontlocation", $_GET['text']);
+header('Content-Type: image/png');
+imagepng($image);
+?> 
