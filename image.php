41765bcc1d3373f47014d655b96a63dda52eca25
AthallahDzaki <discordgg5@gmail.com>
2020-11-09 13:32:41 +0700

    Edit image.php

--- image.php
+++ image.php
@@ -1,9 +1,5 @@
 <?php
+require "include/text_to_image.php";
 if(empty($_GET['text'])) echo 'Error Text Not Found', die;
-$image = imagecreate(200,80);
-$bg = imagecolorallocatealpha($image,255,255,255,0);
-$black = imagecolorallocate($image,255,255,255);
-imagettftext($image,11, 0, x, y, $black, "fontlocation", $_GET['text']);
-header('Content-Type: image/png');
-imagepng($image);
+TextToImage($_GET['text']);
 ?> 
